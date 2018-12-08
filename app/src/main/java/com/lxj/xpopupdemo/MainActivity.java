9f0a8c54f008c95017ed3ddb444aafa6413fcec9
li-xiaojun <16167479@qq.com>
2018-12-09 01:07:13 +0800

    添加展示基础弹窗

--- app/src/main/java/com/lxj/xpopupdemo/MainActivity.java
+++ app/src/main/java/com/lxj/xpopupdemo/MainActivity.java
@@ -2,6 +2,9 @@ package com.lxj.xpopupdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.lxj.xpopup.XPopup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnCenter).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XPopup.get(MainActivity.this).show();
+            }
+        });
     }
 }
