8d0f8628151318eec9bfe6ed3cc784e4bc13059a
li-xiaojun <16167479@qq.com>
2019-04-15 15:11:32 +0800

    修复局部阴影弹窗点击外部不消失的问题

--- app/src/main/java/com/lxj/xpopupdemo/fragment/ImageViewerDemo.java
+++ app/src/main/java/com/lxj/xpopupdemo/fragment/ImageViewerDemo.java
@@ -9,10 +9,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
-import com.blankj.utilcode.constant.PermissionConstants;
-import com.blankj.utilcode.util.PermissionUtils;
-import com.blankj.utilcode.util.ToastUtils;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
@@ -24,7 +20,6 @@ import com.lxj.xpopup.core.ImageViewerPopupView;
 import com.lxj.xpopup.interfaces.OnSrcViewUpdateListener;
 import com.lxj.xpopup.interfaces.XPopupImageLoader;
 import com.lxj.xpopupdemo.R;
-
 import java.io.File;
 import java.util.ArrayList;
 
--- library/src/main/java/com/lxj/xpopup/widget/PartShadowContainer.java
+++ library/src/main/java/com/lxj/xpopup/widget/PartShadowContainer.java
@@ -6,6 +6,7 @@ import android.graphics.Rect;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -21,12 +22,13 @@ import com.lxj.xpopup.util.XPopupUtils;
  */
 public class PartShadowContainer extends FrameLayout {
     public boolean isDismissOnTouchOutside = true;
+
     public PartShadowContainer(@NonNull Context context) {
         super(context);
     }
 
     public PartShadowContainer(@NonNull Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public PartShadowContainer(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -35,13 +37,16 @@ public class PartShadowContainer extends FrameLayout {
 
     private float x, y;
     private long downTime;
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         // 计算implView的Rect
         View implView = getChildAt(0);
-        Rect implViewRect = new Rect();
-        implView.getLocalVisibleRect(implViewRect);
-        if(!XPopupUtils.isInRect(event.getX(), event.getY(), implViewRect)){
+        int[] location = new int[2];
+        implView.getLocationInWindow(location);
+        Rect implViewRect = new Rect(location[0], location[1], location[0] + implView.getMeasuredWidth() - (int)getTranslationY(),
+                location[1] + implView.getMeasuredHeight() - (int)getTranslationY());
+        if (!XPopupUtils.isInRect(event.getX(), event.getY(), implViewRect)) {
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
                     x = event.getX();
@@ -51,10 +56,10 @@ public class PartShadowContainer extends FrameLayout {
                 case MotionEvent.ACTION_UP:
                     float dx = event.getX() - x;
                     float dy = event.getY() - y;
-                    float distance = (float) Math.sqrt(Math.pow(dx,2) + Math.pow(dy, 2));
-                    if(distance< ViewConfiguration.get(getContext()).getScaledTouchSlop() && (System.currentTimeMillis() - downTime)<350){
-                        if (isDismissOnTouchOutside){
-                            if(listener!=null)listener.onClickOutside();
+                    float distance = (float) Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+                    if (distance < ViewConfiguration.get(getContext()).getScaledTouchSlop() && (System.currentTimeMillis() - downTime) < 350) {
+                        if (isDismissOnTouchOutside) {
+                            if (listener != null) listener.onClickOutside();
                         }
                     }
                     x = 0;
@@ -65,8 +70,10 @@ public class PartShadowContainer extends FrameLayout {
         }
         return true;
     }
+
     private OnClickOutsideListener listener;
-    public void setOnClickOutsideListener(OnClickOutsideListener listener){
+
+    public void setOnClickOutsideListener(OnClickOutsideListener listener) {
         this.listener = listener;
     }
 }
