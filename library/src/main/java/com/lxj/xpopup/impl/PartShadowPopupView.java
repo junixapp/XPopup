bbe890692089f473d76403c5e2e2758d3b04c19e
li-xiaojun <16167479@qq.com>
2019-04-29 21:26:11 +0800

    完美处理所有方向的横屏显示

--- library/src/main/java/com/lxj/xpopup/impl/PartShadowPopupView.java
+++ library/src/main/java/com/lxj/xpopup/impl/PartShadowPopupView.java
@@ -10,6 +10,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 
 import com.lxj.xpopup.animator.PopupAnimator;
@@ -44,8 +45,14 @@ public abstract class PartShadowPopupView extends AttachPopupView {
         shadowBgAnimator.targetView = getPopupContentView();
 
         //1. apply width and height
+        int rotation = ((WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRotation();
         ViewGroup.LayoutParams params = getPopupContentView().getLayoutParams();
-        params.width = getMeasuredWidth(); // 满宽
+        if(rotation==0){
+            params.width = getMeasuredWidth(); // 满宽
+        }else if(rotation==1 || rotation==3){
+            params.width = getMeasuredWidth() - (XPopupUtils.isNavBarVisible(getContext()) ? XPopupUtils.getNavBarHeight() : 0)
+                    - XPopupUtils.getStatusBarHeight();
+        }
 
         //1. 获取atView在屏幕上的位置
         int[] locations = new int[2];
