765b48d255e47b3d129b1d07d12e20ecf16ba361
li-xiaojun <16167479@qq.com>
2019-06-06 11:42:42 +0800

    发布1.8.0-x

--- app/src/main/java/com/lxj/xpopupdemo/custom/CustomPartShadowPopupView2.java
+++ app/src/main/java/com/lxj/xpopupdemo/custom/CustomPartShadowPopupView2.java
@@ -1,13 +1,8 @@
 package com.lxj.xpopupdemo.custom;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 
 import com.lxj.xpopup.impl.PartShadowPopupView;
 import com.lxj.xpopupdemo.R;
