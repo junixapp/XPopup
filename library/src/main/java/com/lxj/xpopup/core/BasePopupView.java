dc6a33fb60c8fdd05a06e8070c4ab077f92b47f3
li-xiaojun <16167479@qq.com>
2019-06-13 19:47:53 +0800

    自动设置adjust_resize

--- library/src/main/java/com/lxj/xpopup/core/BasePopupView.java
+++ library/src/main/java/com/lxj/xpopup/core/BasePopupView.java
@@ -6,7 +6,6 @@ import android.graphics.Rect;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -16,7 +15,6 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.FrameLayout;
-
 import com.lxj.xpopup.XPopup;
 import com.lxj.xpopup.animator.EmptyAnimator;
 import com.lxj.xpopup.animator.PopupAnimator;
