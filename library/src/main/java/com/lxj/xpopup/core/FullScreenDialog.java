0c615b20d55aa85196c94fdbf81828c38517552f
li-xiaojun <16167479@qq.com>
2020-07-11 00:27:57 +0800

    发布2.0.7-rc4

--- library/src/main/java/com/lxj/xpopup/core/FullScreenDialog.java
+++ library/src/main/java/com/lxj/xpopup/core/FullScreenDialog.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
