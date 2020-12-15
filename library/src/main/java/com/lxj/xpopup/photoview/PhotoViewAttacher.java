04065584869327432f457929833ec997dbd73d98
li-xiaojun <16167479@qq.com>
2020-12-15 19:56:39 +0800

    发布2.2.11

--- library/src/main/java/com/lxj/xpopup/photoview/PhotoViewAttacher.java
+++ library/src/main/java/com/lxj/xpopup/photoview/PhotoViewAttacher.java
@@ -20,7 +20,6 @@ import android.graphics.Matrix;
 import android.graphics.Matrix.ScaleToFit;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
