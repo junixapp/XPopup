221b196b3a4d391174ddf7f0f36735996ccf1270
tfs_guoshoushuai <158483278@qq.com>
2024-08-26 16:15:27 +0800

    fix：底部List弹窗(带选中效果)深色模式item颜色

--- library/src/main/java/com/lxj/xpopup/impl/BottomListPopupView.java
+++ library/src/main/java/com/lxj/xpopup/impl/BottomListPopupView.java
@@ -112,8 +112,9 @@ public class BottomListPopupView extends BottomPopupView {
                         holder.getView(R.id.check_view).setVisibility(position == checkedPosition ? VISIBLE : GONE);
                         holder.<CheckView>getView(R.id.check_view).setColor(XPopup.getPrimaryColor());
                     }
-                    holder.<TextView>getView(R.id.tv_text).setTextColor(position == checkedPosition ?
-                            XPopup.getPrimaryColor() : getResources().getColor(R.color._xpopup_title_color));
+                    if (position == checkedPosition){
+                        holder.<TextView>getView(R.id.tv_text).setTextColor(XPopup.getPrimaryColor());
+                    }
                     holder.<TextView>getView(R.id.tv_text).setGravity(XPopupUtils.isLayoutRtl(getContext()) ? Gravity.END : Gravity.START);
                 }else {
                     if(holder.getViewOrNull(R.id.check_view)!=null)holder.getView(R.id.check_view).setVisibility(GONE);
